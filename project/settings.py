715330f10d559746576cceca7ba478f1e0bf72b7
gaeul <e0540@hanmail.net>
2022-02-25 10:36:36 +0900

    settings.py바꿈

--- project/settings.py
+++ project/settings.py
@@ -21,9 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+import os, environ
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-wly39ym^u-y+1av#vq8k2x7k^!zcqpe!p_9h0ey13_rl9qmydo"
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
