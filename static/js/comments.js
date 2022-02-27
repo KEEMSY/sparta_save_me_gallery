c9eb7362038d0fe80c23cd55414bb79ace7de7c2
zhqmfkv <sk49229038@gmail.com>
2022-02-27 23:28:32 +0900

    이전커밋 내용 동일
    home.html - 애니메이션 추가, 실제 URL 연결,
    activities - choice.html 추가, 링크 연결, 파일 업로드 및 미리보기 구현, 슬라이드 구현, 라디오박스 구현, API 연결,
    comment.html - 삭제 버튼 추가, 삭제시 이름. 비번 누르는 곳 추가, 제목 만들기 기능구현, DB 내용 넣기, API 연결

---
+++ static/js/comments.js
@@ -0,0 +1,95 @@
+
+
+$(document).ready(function() {
+    var csrftoken = getCookie('csrftoken');
+    $.ajaxSetup({
+        beforeSend: function (xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        }
+    });
+    comment_check();
+
+});
+
+
+
+function comment_check(){
+    let comment_list = document.getElementsByClassName('comment_info')
+    for(let i = 0; i<comment_list.length; i ++){
+
+        let title = comment_list[i].children[1].innerText.split('<br/>')[0].slice(0,10)
+        comment_list[i].children[0].children[0].innerText = title
+        comment_list[i].children[1].innerHTML = comment_list[i].children[1].innerText
+    }
+
+}
+
+
+
+function open_write_box(){
+    document.getElementById('modal_container').style.display = 'block'
+}
+
+function close_write_box(){
+    document.getElementById('modal_container').style.display = 'none'
+    document.getElementById('comment_name').value = ''
+    document.getElementById('comment_password').value = ''
+    document.getElementById('comment_content').value = ''
+}
+
+function save_comment(){
+    let username = document.getElementById('comment_name').value
+    let pwd = document.getElementById('comment_password').value
+    let comment = document.getElementById('comment_content').value
+    comment = comment.replace(/(?:\r\n|\r|\n)/g, '<br/>');
+    $.ajax({
+        type: 'POST',
+        url: '/comments/',
+        data: {
+            'username': username,
+            'password': pwd,
+            'comment': comment,
+        },
+        success: function(response){
+            console.log('msg')
+        }
+    })
+
+    location.reload();
+}
+
+function open_delete_comment_box(id){
+    document.getElementById('modal_delete_container').style.display='block'
+    document.getElementById('delete_target').innerText = id.split('_')[1]
+}
+
+function close_delete_comment_box(){
+    document.getElementById('modal_delete_container').style.display='none'
+    document.getElementById('check_comment_name').value=''
+    document.getElementById('check_comment_password').value=''
+    document.getElementById('delete_target').innerText=''
+}
+
+function delete_comment(){
+
+    let username = document.getElementById('check_comment_name').value
+    let pwd = document.getElementById('check_comment_password').value
+    let target_id = document.getElementById('delete_target').innerText
+    let comment = document.getElementById('comment_content_' + target_id).innerText
+    comment = comment.replace(/(?:\r\n|\r|\n)/g, '<br/>');
+    console.log(comment)
+    $.ajax({
+        type: 'POST',
+        url: '/comments/delete/',
+        data: {
+            'username': username,
+            'password': pwd,
+            'comment': comment,
+        },
+        success: function(response){
+            console.log(response['msg'])
+        }
+    })
+}
