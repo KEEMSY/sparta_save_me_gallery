55a887adddb17e032cac6f17f8444385a9973a9f
gaeul <e0540@hanmail.net>
2022-02-28 21:15:41 +0900

    csrf exempt코드 추가

--- commentapp/views.py
+++ commentapp/views.py
@@ -1,9 +1,12 @@
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
 from commentapp.services.comment_service import get_comment_page, add_comment, delete_comment
 
 # Create your views here.
 
+@csrf_exempt
 def comment_page(request):
     if request.method == 'GET':
         page = request.GET.get('page')
@@ -16,7 +19,7 @@ def comment_page(request):
         add_comment(username, password, comment)
         return JsonResponse({'msg': 'Successfully Saved!'})
 
-
+@csrf_exempt
 def delete(request):
     password = request.POST['password']
     id = request.POST['id']
