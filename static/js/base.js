9256734047c1ed97357cbfe0280fa020606adbdf
zhqmfkv <sk49229038@gmail.com>
2022-02-28 09:29:37 +0900

    model2.html frame 파일 만듦
    댓글 애니메이션 수정
    choice.html 슬라이드 구현 중
    구현중..

--- static/js/base.js
+++ static/js/base.js
@@ -1,3 +1,5 @@
+/* csrf 토큰 관련 */
+
 function getCookie(name) {
     var cookieValue = null;
     if (document.cookie && document.cookie != '') {
@@ -18,3 +20,23 @@ function csrfSafeMethod(method) {
     // these HTTP methods do not require CSRF protection
     return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
 }
+
+/* 언더바 */
+
+let underline = document.getElementById('under_bar')
+let menu_list = document.querySelectorAll('.nav_context_box a')
+console.log(underline)
+console.log(menu_list)
+
+menu_list.forEach((menu)=>
+    menu.addEventListener('mouseover ',(e)=> indicator(e)))
+
+function indicator(e){
+    console.log('event on!')
+    underline.style.left=e.currentTarget.offsetLeft +"px";
+    underline.style.width = e.currentTarget.offsetWidth = "px";
+    underline.style.top = e.currentTarget.offsetTop + e.currentTarget.offsetHeight + "px";
+}
+
+/* 이미지 옮기기! */
+
--- static/js/comments.js
+++ static/js/comments.js
@@ -93,3 +93,4 @@ function delete_comment(){
         }
     })
 }
+
