5f0eeb9abdeb9f44e4e5a02f76de154e68e3e15b
gaeul <e0540@hanmail.net>
2022-02-28 22:58:03 +0900

    예외처리 코드 추가

--- commentapp/services/comment_service.py
+++ commentapp/services/comment_service.py
@@ -11,7 +11,7 @@ def get_comment_page(page:int)-> Page:
 
 def add_comment(username:str, password:str, comment:str) -> Comment:
     profile_img = "/static/img/"+str(random.randrange(1,9))+".jpg"
-    if username == '' or password == '':
+    if username == '' or password == '' or comment == '':
         msg = 'Please check the blank!'
         return msg
     else:
@@ -24,20 +24,19 @@ def delete_comment(username:str ,password:str, id:str):
     found_comment = Comment.objects.filter(pk=id).get()
     #유저이름과 댓글내용 일치하는 댓글을 찾아서 found_comment에 담음
     if found_comment: #found_comment가 있으면..
-        if password == found_comment.password:
+        if str(password) == found_comment.password:
             found_comment.delete()
             msg = 'Successfully deleted'
+            return msg
+        elif username == '' or password == '': #username 이나 password가 없니?
+            msg = 'Please check the blank!'
             return msg
         else:#비밀번호가 일치하지 않는다면
             msg = 'Incorrect password'
             return msg
     else:# found_comment가 없다면?
-        if username == '' or password == '': #username 이나 password가 없니?
-            msg = 'Please check the blank!'
-            return msg
-        else:
-            msg = 'Wrong approach'
-            return msg
+         msg = 'Wrong approach'
+         return msg
 
 
 
